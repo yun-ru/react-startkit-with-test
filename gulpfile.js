2e9f95d97267142b367979f227485f471aeb3e54
myblue <mp4bj6ruby@gmail.com>
2016-08-21 16:28:06 +0800

    modify dist to build and also include js(dir)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
+var replace = require('gulp-replace');
 var webpack = require('webpack-stream');
 gulp.task('webpack', function() {
     return gulp.src('src/app.js')
         .pipe(webpack( require('./webpack.config.js') ))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('build/js'));
 });
+
+gulp.task('index',function(){
+    return gulp.src('index.html')
+        .pipe(replace(/assets/g, 'js'))
+        .pipe(gulp.dest('build/'));
+})
 
 gulp.task("watch",function(){
     gulp.watch('./src/**/*',['webpack'])
