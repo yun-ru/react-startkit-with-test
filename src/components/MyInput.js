eb66f0b4bb03dbf67f3ff4c14ce09c4161b390aa
Ruby Chen <ruby@yun-ru.com>
2016-08-23 16:40:25 +0800

    rename UserComponent and MyInputComponent; added user list with create and remove function

---
+++ src/components/MyInput.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+
+
+class User extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            myText: ""
+        }
+    }
+    componentDidMount() {
+        // const el = findDOMNode(this);
+    }
+    handleChange(e) {
+        this.setState({myText: e.target.value})
+    }
+    handleClick() {
+        this.setState({myText: ""},()=>{
+            this.refs.myInput.focus();
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>The Text: {this.state.myText}</h3>
+                <p ref="myGood">good</p>
+                <input ref="myInput" value={this.state.myText} onChange={this.handleChange.bind(this)} type="text"/>
+                <button onClick={this.handleClick.bind(this)}>Clear it!</button>
+            </div>
+        )
+    }
+}
+
+
+export default User;
