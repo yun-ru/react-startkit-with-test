bf180fcc4640592469f922f0f90c44755c69980a
Ruby Chen <ruby@yun-ru.com>
2016-08-23 15:42:15 +0800

    added level sign and num

--- src/components/utils/RepeatEle.js
+++ src/components/utils/RepeatEle.js
@@ -5,6 +5,6 @@ export default (props) =>{
     for(let i=0; i<props.times; i++){
         arr.push(props.children);
     }
-    return <div> { arr.map(item=>{ return item }) } </div>
+    return <span> { arr.map(item=>{ return item }) } </span>
 };
 
